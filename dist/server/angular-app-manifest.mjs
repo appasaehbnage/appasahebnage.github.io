e9c5c006b1a04ee26e0598c96668d55c89570d8e
Appasaheb Nage <appasahebnage776@gmail.com>
2024-12-30 07:53:59 +0530

    fix: static image path

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -19,11 +19,11 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 2332, hash: '65b65d466486da3dcaac8f50da7482bfde932fac8a37ff0e9ef22f973ec93f3a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1074, hash: '8e262211f4e2b7fbabdea3468d613add3e576f0196f1adbe6a01d9a8333885ce', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    '#about/index.html': {size: 33912, hash: 'f75bd8a2e07fdc398b43788a93240aa9f81478c4f2b89271b8197871aca2de96', text: () => import('./assets-chunks/#about_index_html.mjs').then(m => m.default)},
-    '#experience/index.html': {size: 33912, hash: 'f75bd8a2e07fdc398b43788a93240aa9f81478c4f2b89271b8197871aca2de96', text: () => import('./assets-chunks/#experience_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 33912, hash: 'f75bd8a2e07fdc398b43788a93240aa9f81478c4f2b89271b8197871aca2de96', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2332, hash: 'b5bf7a59d187d1e8d6633a9bcd24bc4089b490f842ddae9cfe979b4d0b8b4131', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1074, hash: 'ec4d37814f523b67cdedc4db575a21c7a176d73eea9a7c00745d22fbbaea79f6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    '#about/index.html': {size: 34036, hash: '8e64f943436d6935972773aa2e66e4a26c759529def30cf82810dd50974572e8', text: () => import('./assets-chunks/#about_index_html.mjs').then(m => m.default)},
+    '#experience/index.html': {size: 34036, hash: '8e64f943436d6935972773aa2e66e4a26c759529def30cf82810dd50974572e8', text: () => import('./assets-chunks/#experience_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 34036, hash: '8e64f943436d6935972773aa2e66e4a26c759529def30cf82810dd50974572e8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-U3557VHC.css': {size: 36668, hash: 'CjMfYSXyF3k', text: () => import('./assets-chunks/styles-U3557VHC_css.mjs').then(m => m.default)}
   },
 };
