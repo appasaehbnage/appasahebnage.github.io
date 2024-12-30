934ba6abc3539a124fdd80515f4f915b4d6c9a8c
Appasaheb Nage <appasahebnage776@gmail.com>
2024-12-30 08:08:24 +0530

    fix: updated the header image path

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 2332, hash: '18812f51a3d0cd4c766a37d4aba738edec60d6d0256cb152a6c4ce8a27f38254', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1074, hash: '3e6a26b44720b65e3728104d25362b51a50751c359a3d7807b3bdb21cfcc81ab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 33890, hash: '6c4217da58ab9e70d54786a58620002733b5d824c6e1ed3a0fec715fa9101047', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2332, hash: '67289d7a3fe7258e168659b3a6c459056ec4a08bef1f672ddeb93b7f80240f91', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1074, hash: '339e96677ea7361e98caf580cfc2d33a41383892ce0d874595bbbbddef8af71d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 33871, hash: 'e781507aeec8b6afed6378c8316b56afab1dbced68eb015ec896e24dedb59e38', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-U3557VHC.css': {size: 36668, hash: 'CjMfYSXyF3k', text: () => import('./assets-chunks/styles-U3557VHC_css.mjs').then(m => m.default)}
   },
 };
